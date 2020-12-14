192fab9bdaeed4ce61959e23c33f37001b1f4d0c
Suat Ural <sural@windowslive.com>
2020-12-14 01:06:57 -0500

    Login and Home Components

---
+++ frontend/src/app/app.routing.ts
@@ -0,0 +1,12 @@
+import { RouterModule, Routes } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
+
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+export const AppRoutes: Routes = [
+	{ path: '', component: LoginComponent },
+	{ path: 'home', component: HomeComponent }
+];
+
+export const ROUTING: ModuleWithProviders<any> = RouterModule.forRoot(AppRoutes);
