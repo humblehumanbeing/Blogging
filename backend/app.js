0f6ce3fa7bdcd72dc53080e548d23486be4a4359
Suat Ural <sural@windowslive.com>
2020-12-16 21:15:57 -0500

    Updates to make the API call

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const url = 'mongodb://localhost/blogDb';
 
 const User = require('./model/user');
+const Post = require('./model/post');
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended : false}))
@@ -45,6 +46,19 @@ app.post('/api/user/create', (req, res) => {
 			return res.status(200).json({
 				status: 'success',
 				data: res
+			})
+		})
+	});
+})
+
+app.post('/api/post/getAllPost', (req, res) => {
+	mongoose.connect(url, { useMongoClient: true } , function(err){
+		if(err) throw err;
+		Post.find({},[],{ sort: { _id: -1 } },(err, doc) => {
+			if(err) throw err;
+			return res.status(200).json({
+				status: 'success',
+				data: doc
 			})
 		})
 	});
