be7e509c23ed527524ec9bb84aea1479657a98d5
Suat Ural <sural@windowslive.com>
2021-01-09 23:17:44 -0500

    Define an observable in CommonService

--- frontend/src/app/service/common.service.ts
+++ frontend/src/app/service/common.service.ts
@@ -5,9 +5,20 @@ import { Subject }    from 'rxjs/Subject';
 export class CommonService {
 
 	public postAdded_Observable = new Subject();
+	public postEdit_Observable = new Subject();
+	public post_to_be_edited;
 
 	constructor(){
+		this.post_to_be_edited = new Post();
+	}
 
+	notifyPostEdit(){
+		this.postEdit_Observable.next();
+	}
+
+	setPostToEdit(post: Post){
+		this.post_to_be_edited = post;
+		this.notifyPostEdit();
 	}
 
 	notifyPostAddition(){
