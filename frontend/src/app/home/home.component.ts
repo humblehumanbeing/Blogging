f37adb77109df15f3c0f96d11eb0d958b1a176e9
Suat Ural <sural@windowslive.com>
2021-01-09 23:20:45 -0500

    Subscribe to the postEdit_Observable from CommonService

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { CommonService } from '../service/common.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+	@ViewChild('addPost') addBtn: ElementRef;
+
+	constructor(private commonService: CommonService){
+		
+		this.commonService.postEdit_Observable.subscribe(res => {
+			this.addBtn.nativeElement.click();
+		});
+
+	}
   
 }
